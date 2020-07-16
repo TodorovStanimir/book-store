15af6ad76f02c81473e978dd27c2d4da54d780c0
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-07-16 12:07:22 +0300

    feat: setup the config file

---
+++ REST API/config/config.js
@@ -0,0 +1,20 @@
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+    development: {
+        port: process.env.PORT || 3000,
+        dbUrl: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-fyce0.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`,
+        cookieSecret: process.env.COOKIE_SECRET,
+        jwtSecret: process.env.JWT_SECRET,
+        saltRounds: Number(process.env.SALT_ROUNDS)
+    },
+    production: {
+        port: process.env.PORT || 3000,
+        dbUrl: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-fyce0.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`,
+        cookieSecret: process.env.COOKIE_SECRET,
+        jwtSecret: process.env.JWT_SECRET,
+        saltRounds: Number(process.env.SALT_ROUNDS)
+    }
+}
+
+module.exports = config[env];
