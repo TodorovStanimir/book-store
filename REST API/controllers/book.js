0d2741985e8ba1063484574c2069360626958fdc
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-07-16 14:29:20 +0300

    feat: created book controllers

---
+++ REST API/controllers/book.js
@@ -0,0 +1,48 @@
+const { Book, User } = require('../models');
+
+module.exports = {
+    get: async (req, res, next) => {
+        try {
+            const books = await Book.find().popupale('creator');
+            res.send(books)
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    post: async (req, res, next) => {
+        const { title, author, description, genres, year, publisher, price, imageUrl } = req.body;
+        const { _id } = req.user;
+
+        try {
+            const createdBook = await Book.create({ title, author, description, genres, year, publisher, price, imageUrl, creator: _id });
+            const updatedUser = await User.updateOne({ _id }, { $push: { books: createdBook } });
+
+            res.status(201).send(createdBook);
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    put: async (req, res, next) => {
+        const id = req.params.id;
+        const { title, author, description, genres, year, publisher, price, imageUrl } = req.body;
+        try {
+            const updatedBook = await Book.updateOne({ _id: id }, { title, author, description, genres, year, publisher, price, imageUrl });
+            res.status(200).send(updatedBook)
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    delete: async (req, res, next) => {
+        const id = req.params.id;
+
+        try {
+            const removedBoook = await Book.deleteOne({ _id: id });
+            res.status(204).send(removedBoook);
+        } catch (error) {
+            next(error)
+        }
+    }
+};
