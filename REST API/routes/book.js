7fb5321a6360f05f32514e1336661936695ed080
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-07-17 01:25:23 +0300

    fix: add validator for books

--- REST API/routes/book.js
+++ REST API/routes/book.js
@@ -2,12 +2,13 @@ const controllers = require('../controllers/');
 const router = require('express').Router();
 
 const { auth } = require('../utils');
+const { bookValidator } = require('../utils/validators');
 
 router.get('/', controllers.book.get);
 
-router.post('/', auth(), controllers.book.post);
+router.post('/', auth(), bookValidator, controllers.book.post);
 
-router.put('/:id', auth(), controllers.book.put);
+router.put('/:id', auth(), bookValidator, controllers.book.put);
 
 router.delete('/:id', auth(), controllers.book.delete);
 
