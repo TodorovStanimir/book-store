782de65b4425c312f095a0eab4d2b284a74f9c52
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-07-16 16:05:32 +0300

    feat: create index js file form models

---
+++ REST API/models/index.js
@@ -0,0 +1,9 @@
+const User = require('./user');
+const Book = require('./book');
+const TokenBlackList = require('./tokenBlackList');
+
+module.exports = {
+    User,
+    Book,
+    TokenBlackList
+};
