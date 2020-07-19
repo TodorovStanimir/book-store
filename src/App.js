3819df3bbbcb33165f18cea3293c2a01351d81b8
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-07-19 21:44:43 +0300

    feat: add footer component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 // import styles from './app.module.css';
 import Header from './components/header'
+import Footer from './components/footer';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import moment from 'moment';
+import styles from './index.module.css'
+
+const Footer = () => {
+    const today = moment(new Date()).format("dddd DD-MMMM-YYYY");
+    console.log(today.toString())
+    return (
+        <footer className={styles.footer}>
+            <p>
+                <b>Book Store @ All rights reserved &copy; {today}</b>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
