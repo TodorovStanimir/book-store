4cbd4ccb898ab4f895929a02e865fec22f526ada
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-07-19 21:20:40 +0300

    feat; add header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import styles from './app.module.css';
+// import styles from './app.module.css';
+import Header from './components/header'
 
 function App() {
   return (
     <div>
-
+      <Header/>
     </div>
   );
 }
