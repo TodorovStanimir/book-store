17a94a40d475e39a2b1ea1d16995845586ced07f
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-07-25 08:31:20 +0300

    feat: integrated InputEl and ValidatorEl and added validation of the form

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import PageLayout from '../../components/page-layout/page-layout'
-import styles from './index.module.css'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import styles from './index.module.css'
+import PageLayout from '../../components/page-layout/page-layout'
+import InputEl from '../../components/input-el';
+import ValidatorEl from '../../components/validator-el';
+import { emailValidator, passwordValidator } from '../../utils/validators'
 
-const Login = () => {
-    const isValid = true;
+const Login = (props) => {
+    const [inputData, setInputState] = useState({ email: '', password: '' });
+
+    const [validators, setValidators] = useState({
+        email: true,
+        password: true
+    });
+
+    const onChange = (e) => {
+        const currentValidator = {
+            email: emailValidator,
+            password: passwordValidator
+        }[e.target.name].test(e.target.value)
+
+        setInputState({ ...inputData, [e.target.name]: e.target.value })
+        setValidators({ ...validators, [e.target.name]: currentValidator })
+    }
+
+    const { email, password } = inputData;
+    const { email: correctEmail, password: correctPassword } = validators;
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(email, password);
+    }
+
     return (
         <PageLayout>
             <div className={styles.container}>
@@ -12,62 +39,43 @@ const Login = () => {
                     <div className="col-lg-4"></div>
                     <div className="col-lg-4">
                         <div className={styles['form-container']}>
-                            <form>
-                                <div className={styles['input-group']}>
-                                    <div className="input-group-prepend">
-                                        <span className={styles['span-el']}>
-                                            <i className="fa fa-user"></i>
-                                        </span>
-                                    </div>
-                                    <input
-                                        type="text"
-                                        className={isValid ? styles.valid : styles.invalid}
-                                        name="email"
-                                        placeholder="E-mail"
-                                    />
-                                </div>
-
-                                {!isValid
-                                    ? <div>
-                                        <div className={styles['info-field']}>
-                                            This field is required!
-                                    </div>
-                                        <div className={styles['info-field']}>
-                                            Shoud be in format Xxxxx Xxxxx
-                                    </div>
-                                    </div>
-                                    : null
-                                }
-                                <div className={styles['input-group']}>
-                                    <div className="input-group-prepend">
-                                        <span className={styles['span-el']}>
-                                            <i className="fa fa-lock"></i>
-                                        </span>
-                                    </div>
-                                    <input
-                                        type="password"
-                                        className={isValid ? styles.valid : styles.invalid}
-                                        name="password"
-                                        placeholder="Password"
-                                    />
-                                </div>
-                                {!isValid
-                                    ? <div>
-                                        <div className={styles['info-field']}>
-                                            This field is required!
-                            </div>
-                                        <div className={styles['info-field']}>
-                                            Password shoud consists between 3 and 16 symbols: letters and digits!
-                            </div>
-                                    </div>
-                                    : null
-                                }
+                            <form onSubmit={onSubmit}>
+                                <InputEl
+                                    classNameDivEl='input-group'
+                                    classNameSpanEl='span-el'
+                                    classNameIEl='fa fa-envelope'
+                                    type='email'
+                                    name='email'
+                                    placeholder='E-mail'
+                                    value={email}
+                                    isValid={correctEmail}
+                                    onChange={onChange}
+                                />
+                                <ValidatorEl
+                                    validator={correctEmail}
+                                    message='Email shoud be a valid email address, like example@example.extension!'
+                                />
+                                <InputEl
+                                    classNameDivEl='input-group'
+                                    classNameSpanEl='span-el'
+                                    classNameIEl='fa fa-lock'
+                                    type='password'
+                                    name='password'
+                                    placeholder='Password'
+                                    value={password}
+                                    isValid={correctPassword}
+                                    onChange={onChange}
+                                />
+                                <ValidatorEl
+                                    validator={correctPassword}
+                                    message='Password shoud consists between 3 and 16 symbols: letters and digits!'
+                                />
                                 <div className="form-group">
                                     <button type="submit" className={styles['submit-button']}>
                                         Login in Your account
-                            </button>
+                                    </button>
                                 </div>
-                                <Link className={styles['register-link']} to="/user/register">
+                                <Link className={styles['register-link']} to="/profile/register">
                                     <p>Have not an account? Register here</p>
                                 </Link>
                             </form>
