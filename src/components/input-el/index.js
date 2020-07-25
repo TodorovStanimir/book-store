87b852ebe739cb021d717f29a970abb9b02bea73
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-07-25 08:23:55 +0300

    refactor: create InputEl component

---
+++ src/components/input-el/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import styles from './index.module.css';
+
+const InputEl = (props) => {
+    const {
+        classNameDivEl,
+        classNameSpanEl,
+        classNameIEl,
+        type,
+        name,
+        placeholder,
+        isValid,
+        value,
+        onChange
+    } = props;
+
+    return (
+        <div className={styles[`${classNameDivEl}`]}>
+            <div className="input-group-prepend">
+                <span className={styles[`${classNameSpanEl}`]}>
+                    <i className={`${classNameIEl}`}></i>
+                </span>
+            </div>
+            <input
+                type={type ? type : "text"}
+                className={isValid ? styles.valid : styles.invalid}
+                name={name}
+                placeholder={placeholder}
+                value={value}
+                onChange={e => onChange(e)}
+            />
+        </div>
+    )
+}
+
+export default InputEl
