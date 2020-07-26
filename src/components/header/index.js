106382d327eacfdb22106968a13b68324a836aca
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-07-26 21:56:15 +0300

    feat: added UserContext to the header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { useContext, Fragment } from 'react';
 import styles from './index.module.css';
 import logo from '../../images/logo.png'
 import { Link } from 'react-router-dom';
+import UserContext from '../../Context';
 
 const Header = () => {
+    const { isLoggedIn, user } = useContext(UserContext);
     return (
         <div className={styles.sticky}>
             <div className={styles.navbar}>
                 <img className={styles['nav-image']} src={logo} alt="BookStore" />
-                <li className={styles['nav-item']}>
-                    <Link to="/profile/profile" className={styles['nav-link']}>Hello, user</Link>
-                </li>
-                <li className={styles['nav-item']}>
-                    <Link to="/profile/register" className={styles['nav-link']}>Register</Link>
-                </li>
-                <li className={styles['nav-item']}>
-                    <Link to="/profile/login" className={styles['nav-link']}>Login</Link>
-                </li >
-                <li className={styles['nav-item']}>
-                    <Link to="/profile/logout" className={styles['nav-link']}>Logout</Link>
-                </li >
-                <li className={styles['nav-item']}>
-                    <Link to="/profile/profile" className={styles['nav-link']}>Profile</Link>
-                </li >
-                <li className={styles['nav-item']}>
-                    <Link to="/books/create" className={styles['nav-link']}>Create book</Link>
-                </li >
-                <li className={styles['nav-item']}>
-                    <Link to="/books/user" className={styles['nav-link']}>My books</Link>
-                </li >
+
+                {isLoggedIn
+                    ? <Fragment>
+                        <li className={styles['nav-item']}>
+                            <Link to="/profile/profile" className={styles['nav-link']}>Hello, {user && user.username}</Link>
+                        </li>
+                        <li className={styles['nav-item']}>
+                            <Link to="/profile/logout" className={styles['nav-link']}>Logout</Link>
+                        </li >
+                        <li className={styles['nav-item']}>
+                            <Link to="/profile/profile" className={styles['nav-link']}>Profile</Link>
+                        </li >
+                        <li className={styles['nav-item']}>
+                            <Link to="/books/create" className={styles['nav-link']}>Create book</Link>
+                        </li >
+                        <li className={styles['nav-item']}>
+                            <Link to="/books/user" className={styles['nav-link']}>My books</Link>
+                        </li >
+                    </Fragment>
+                    : <Fragment>
+                        <li className={styles['nav-item']}>
+                            <Link to="/profile/register" className={styles['nav-link']}>Register</Link>
+                        </li>
+                        <li className={styles['nav-item']}>
+                            <Link to="/profile/login" className={styles['nav-link']}>Login</Link>
+                        </li >
+                    </Fragment>
+                }
+
             </div >
         </div >
     )
