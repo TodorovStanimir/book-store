6e96a995401f6d18103a7183ec0305f89dd12dce
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-07-26 16:37:49 +0300

    refactor: changed the notification component to presents objects and arrays

--- src/components/notification/index.js
+++ src/components/notification/index.js
@@ -3,15 +3,27 @@ import React from 'react';
 import styles from './undex.module.css';
 
 const Notification = ({ show, message }) => {
-    const msg = {
-        'Unauthorized': 'Invalid user e-mail or password! Please try again!'
-    }[message]
-    return (
-        show &&
-             <div className={styles.loading}>
+
+    if (Array.isArray(message)) {
+        // const msg = message.map((err,ind) => <p key={ind}>${err.msg}</p>
+        return (
+            show &&
+            <div className={styles.loading}>
+                {message.map((err,ind) => <div key={ind}>{err.msg}</div>)}
+            </div>
+        )
+    } else {
+        const msg = {
+            'Unauthorized': 'Invalid user e-mail or password! Please try again!'
+        }[message]
+        return (
+            show &&
+            <div className={styles.loading}>
                 {msg}
             </div>
-    )
+        )
+    }
+
 }
 
 export default Notification;
