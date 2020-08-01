4c31be7acfb35e2603f29f034540d6ee69b2e421
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-08-01 09:54:24 +0300

    style: changed style of header and loader

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from './index.module.css'
 
 const Loader = (props) => {
@@ -6,8 +6,13 @@ const Loader = (props) => {
     return props.showingLoader
         ? (
             <div className={styles.loading}>
-                <div className={styles.dot} />
-                <div className={styles.dot} />
+                {window.screen.width > 400
+                    ? <Fragment>
+                        <div className={styles.dot} />
+                        <div className={styles.dot} />
+                    </Fragment>
+                    : null
+                }
                 <div className={styles.dot} />
                 <div className={styles.dot} />
                 <div className={styles.dot} />
