67f32507524aa00e44e777e820139f59dcd6824f
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-08-05 00:44:23 +0300

    feat: added function-button component

---
+++ src/components/function-button/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './index.module.css';
+
+const FunctionButton = ({ funct, icon, styleDivEl, styleBtnEl, bookId }) => {
+    return (
+        <div className={styles[styleDivEl]}>
+            <button className={styles[styleBtnEl]} onClick={() => funct(bookId)}>
+                <i className={icon}></i>
+            </button>
+        </div>
+    )
+}
+
+export default FunctionButton;
