7ab2f00f522310df4e98744afa692e648b886060
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-08-05 21:34:14 +0300

    feat: added route guards

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Redirect } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import Books from '../../pages/book-all';
@@ -9,22 +9,37 @@ import Login from '../../pages/login';
 import Register from '../../pages/register';
 import Profile from '../../pages/profile';
 import NotFoundRoute from '../../pages/not-found'
+import { UserContext } from '../../Context';
 
 const Navigation = () => {
+    const { isLoggedIn } = useContext(UserContext);
+    console.log('isUserLogged: ', isLoggedIn)
     return (
         <Router>
             <Switch>
                 <Route path="/" exact component={Books} />
                 <Route path="/books/all" component={Books} />
-                <Route path="/books/create" component={CreateBook} />
-                <Route path="/books/edit/:id" component={CreateBook} />
-                <Route path="/books/details/:id" component={BookDetails} />
-                <Route path="/profile/login" component={Login} />
-                <Route path="/profile/register" component={Register} />
-                <Route path="/profile/profile" component={Profile} />
+                <Route path="/books/create">
+                    {isLoggedIn ? (<CreateBook />) : (<Redirect to="/profile/login" />)}
+                </Route>
+                <Route path="/books/edit/:id">
+                    {isLoggedIn ? (<CreateBook />) : (<Redirect to="/profile/login" />)}
+                </Route>
+                <Route path="/books/details/:id">
+                    {isLoggedIn ? (<BookDetails />) : (<Redirect to="/profile/login" />)}
+                </Route>
+                <Route path="/profile/login">
+                    {!isLoggedIn ? (<Login />) : (<Redirect to="/books/all" />)}
+                </Route>
+                <Route path="/profile/register">
+                    {!isLoggedIn ? (<Register />) : (<Redirect to="/books/all" />)}
+                </Route>
+                <Route path="/profile/profile">
+                    {isLoggedIn ? (<Profile />) : (<Redirect to="/profile/login" />)}
+                </Route>
                 <Route component={NotFoundRoute} />
             </Switch>
-        </Router>
+        </Router >
     )
 }
 export default Navigation
